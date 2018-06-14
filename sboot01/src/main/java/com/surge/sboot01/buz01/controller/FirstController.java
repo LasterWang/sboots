222fb4fdcd578e915af59234c3f24cc9e4f72e22
wenllness <wenllness@DESKTOP-1B4A1LU>
2018-06-14 15:56:11 +0800

    dddd

---
+++ sboot01/src/main/java/com/surge/sboot01/buz01/controller/FirstController.java
@@ -0,0 +1,22 @@
+package com.surge.sboot01.buz01.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class FirstController {
+	@RequestMapping("/")
+	@ResponseBody
+	String home()
+	{
+		return "hello";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FirstController.class, args);
+	}
+}
